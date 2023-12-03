83f49402dc11050374b2b00d514140599ea08b44
Mike <msthygesen16@gmail.com>
2023-12-03 16:21:34 +1100

    readme

---
+++ day-01/target/debug/deps/part2-5a32241940fd9a19.d
@@ -0,0 +1,6 @@
+C:\Users\micha\Documents\Code\advent-of-code-2023\day-01\target\debug\deps\part2-5a32241940fd9a19.rmeta: src\bin\part2.rs src\bin\./input2.txt
+
+C:\Users\micha\Documents\Code\advent-of-code-2023\day-01\target\debug\deps\part2-5a32241940fd9a19.d: src\bin\part2.rs src\bin\./input2.txt
+
+src\bin\part2.rs:
+src\bin\./input2.txt:
---
+++ day-01/target/debug/part2.d
@@ -0,0 +1,1 @@
+C:\Users\micha\Documents\Code\advent-of-code-2023\day-01\target\debug\part2.exe: C:\Users\micha\Documents\Code\advent-of-code-2023\day-01\src\bin\input2.txt C:\Users\micha\Documents\Code\advent-of-code-2023\day-01\src\bin\part2.rs
